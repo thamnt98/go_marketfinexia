43448eb4c910fd149b9a34e8c7a1f228b5288f00
Nguyen Thi Tham <thamnt@hblab.vn>
2020-11-25 10:28:56 +0700

    add image need help to side bar

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -3,7 +3,7 @@
 				
         <div class="sidebar-header">
             <div class="sidebar-title">
-                Navigation
+                <img src="{{ asset('image/icon_offline_0.jpeg') }}">
             </div>
             <div class="sidebar-toggle hidden-xs" data-toggle-class="sidebar-left-collapsed" data-target="html" data-fire-event="sidebar-left-toggle">
                 <i class="fa fa-bars" aria-label="Toggle sidebar"></i>
