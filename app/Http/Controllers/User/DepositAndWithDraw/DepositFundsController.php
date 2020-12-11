d9aa77b8e74371a9f595aec68aebb57551a00a81
Nguyen Thi Tham <thamnt@hblab.vn>
2020-12-11 13:15:09 +0700

    init bepay

--- app/Http/Controllers/User/DepositAndWithDraw/DepositFundsController.php
+++ app/Http/Controllers/User/DepositAndWithDraw/DepositFundsController.php
@@ -4,11 +4,21 @@ namespace App\Http\Controllers\User\DepositAndWithDraw;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use GuzzleHttp\Client;
 
 class DepositFundsController extends Controller
 {
     public function main()
     {
-        return view('withdrawanddeposit.deposit_funds');
+        $banks = [];
+        $merchantId = config('bepay.merchant_id');
+        $secretKey = config('bepay.secret_key');
+        $sign = md5($merchantId . $secretKey);
+        $endpoint = "https://bepay2.com/api/v2/payment/listBank?merchant_id=" . $merchantId . "&sign=" . $sign;
+        $client = new Client();
+        $response = $client->request('GET', $endpoint);
+        $result = json_decode($response->getBody());
+        $banks = $result->data;
+        return view('withdrawanddeposit.deposit_funds', compact('banks'));
     }
 }
