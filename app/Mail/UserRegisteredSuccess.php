8dc017d5a3781fdd56744289de2f55cfb063a9ca
Nguyen Tham <tham27011@gmail.com>
2020-12-24 23:32:18 +0700

    refactor code

--- app/Mail/UserRegisteredSuccess.php
+++ app/Mail/UserRegisteredSuccess.php
@@ -40,7 +40,7 @@ class UserRegisteredSuccess extends Mailable
     public function build()
     {
         return $this->view('mail.registersuccess')
-        ->subject('Fwd: Chào mừng bạn đến với Gemi Broker!')
+        ->subject('Chào mừng bạn đến với Gemi Broker!')
         ->with([
             'firstName' => $this->user['first_name'],
             'lastName' => $this->user['last_name'],
