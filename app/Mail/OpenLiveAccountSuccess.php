5e86b69281014866e4d934c89417194c22c8ff2d
Nguyen Thi Tham <thamnt@hblab.vn>
2021-01-19 09:02:22 +0700

    refactor email live account

--- app/Mail/OpenLiveAccountSuccess.php
+++ app/Mail/OpenLiveAccountSuccess.php
@@ -48,7 +48,7 @@ class OpenLiveAccountSuccess extends Mailable
                 'lastName' => $this->user['last_name'],
                 'login' => $this->account['login'],
                 'password' => $this->password,
-                'leverage' => config('mt4.leverage')[$this->account['leverage']]
+                'leverage' => $this->account['leverage']
             ]);
     }
 }
