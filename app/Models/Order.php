dd00605d015ead4f0383a28bf18c5fdd4a40e34b
Nguyen Thi Tham <thamnt@hblab.vn>
2021-01-28 17:59:27 +0700

    Revert "add column is_staff for some table"
    This reverts commit 80cdca3acf539b3a3c49fb573dd4d16471d34b87.

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 class Order extends Model
 {
     use SoftDeletes;
-
+    
     protected $fillable = [
         'user_id',
         'amount_money',
@@ -18,8 +18,7 @@ class Order extends Model
         'sign',
         'created_at',
         'updated_at',
-        'type',
-        'is_staff'
+        'type'
     ];
 
 }
