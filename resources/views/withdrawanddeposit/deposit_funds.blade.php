c306e355d7db3aea1dcd52364850f6ce77406b67
thamnt98 <tham27011@gmail.com>
2021-04-25 14:28:35 +0700

    mo cong tien thanh toan bepay

--- resources/views/withdrawanddeposit/deposit_funds.blade.php
+++ resources/views/withdrawanddeposit/deposit_funds.blade.php
@@ -27,62 +27,64 @@
                         <div class="inner-body mg-main">
                             <div class="row mg-files" data-sort-destination data-sort-id="media-gallery">
                                 <div class="isotope-item document col-sm-6 col-md-4 col-lg-2">
-{{--                                    <a href="{{ route('deposit.bepay') }}">--}}
-{{--                                        <div class="thumbnail">--}}
-{{--                                            <div class="thumb-preview text-center">--}}
-{{--                                                <img src="{{ asset('image/bepay.jpg') }}" class="img-thumnail" alt="Project" style="height:110px">--}}
-{{--                                                <div class="mg-thumb-options"></div>--}}
-{{--                                            </div>--}}
-{{--                                        </div>--}}
-{{--									</a>--}}
-                                    <a href="#">
+                                    <a href="{{ route('deposit.bepay') }}">
                                         <div class="thumbnail">
-                                            <div class="thumb-preview text-center" style="margin-top:20px">
-                                                <img src="{{ asset('image/tether.png') }}" class="img-thumnail" alt="Project">
-                                                <div class="mg-thumb-options"></div>
-                                            </div>
-                                        </div>
-                                    </a>
-								</div>
-								<div class="isotope-item document col-sm-6 col-md-4 col-lg-2">
-                                    <a href="#vifa" class="modal-with-form">
-                                        <div class="thumbnail">
-                                            <div class="thumb-preview" style="padding-top: 34px; padding-bottom:34px">
-                                                <img src="{{ asset('image/vifapay.jpg') }}" class="img-responsive" alt="Project">
+                                            <div class="thumb-preview text-center">
+                                                <img src="{{ asset('image/bepay.jpg') }}" class="img-thumnail" alt="Project" style="height:110px">
                                                 <div class="mg-thumb-options"></div>
                                             </div>
                                         </div>
 									</a>
-									<div id="vifa" class="modal-block modal-block-primary mfp-hide">
-										<section class="panel">
-											<div class="panel-body">
-												<form class="ajax-form" class="form-horizontal" method="post" action="{{ route('transfer.vifa') }}" novalidate="novalidate">
-													@csrf
-													<div class="form-group">
-														<label class="col-sm-3 control-label"><b>Amount of money (VND)</b></label>
-														<div class="col-sm-9">
-														<input type="number" name="amount_money"  class="form-control" placeholder="Amount of money" value="{{ old('amount_money') }}"/>
-														<div class="errors errors-amount_money"></div>
-														</div>
-													</div>
-													<br>
-													<div class="form-group">
-														<div class="col-sm-3"></div>
-														<div class="col-sm-9">
-															<button type="submit" class="btn btn-primary">Transfer</button>
-															<button class="btn btn-default modal-dismiss">Cancel</button>
-														</div>
-													</div>
-												</form>
-											</div>
-										</section>
-									</div>
 								</div>
 								<div class="isotope-item document col-sm-6 col-md-4 col-lg-2">
+                                    <a href="#vifa" class="modal-with-form">
+                                        <div class="thumbnail">
+                                            <div class="thumb-preview" style="">
+                                                <img src="{{ asset('image/paynew.jpg') }}" class="img-responsive" alt="Project" style="height: 100%">
+                                                <div class="mg-thumb-options"></div>
+                                            </div>
+                                        </div>
+									</a>
+{{--									<div id="vifa" class="modal-block modal-block-primary mfp-hide">--}}
+{{--										<section class="panel">--}}
+{{--											<div class="panel-body">--}}
+{{--												<form class="ajax-form" class="form-horizontal" method="post" action="{{ route('transfer.vifa') }}" novalidate="novalidate">--}}
+{{--													@csrf--}}
+{{--													<div class="form-group">--}}
+{{--														<label class="col-sm-3 control-label"><b>Amount of money (VND)</b></label>--}}
+{{--														<div class="col-sm-9">--}}
+{{--														<input type="number" name="amount_money"  class="form-control" placeholder="Amount of money" value="{{ old('amount_money') }}"/>--}}
+{{--														<div class="errors errors-amount_money"></div>--}}
+{{--														</div>--}}
+{{--													</div>--}}
+{{--													<br>--}}
+{{--													<div class="form-group">--}}
+{{--														<div class="col-sm-3"></div>--}}
+{{--														<div class="col-sm-9">--}}
+{{--															<button type="submit" class="btn btn-primary">Transfer</button>--}}
+{{--															<button class="btn btn-default modal-dismiss">Cancel</button>--}}
+{{--														</div>--}}
+{{--													</div>--}}
+{{--												</form>--}}
+{{--											</div>--}}
+{{--										</section>--}}
+{{--									</div>--}}
+								</div>
+{{--								<div class="isotope-item document col-sm-6 col-md-4 col-lg-2">--}}
+{{--                                    <a href="#">--}}
+{{--                                        <div class="thumbnail">--}}
+{{--                                            <div class="thumb-preview" style="padding-top:23.5px; padding-bottom:23.5px">--}}
+{{--                                                <img src="{{ asset('image/neteller.jpg') }}" class="img-responsive" alt="Project">--}}
+{{--                                                <div class="mg-thumb-options"></div>--}}
+{{--                                            </div>--}}
+{{--                                        </div>--}}
+{{--                                    </a>--}}
+{{--                                </div>--}}
+                                <div class="isotope-item document col-sm-6 col-md-4 col-lg-2">
                                     <a href="#">
                                         <div class="thumbnail">
-                                            <div class="thumb-preview" style="padding-top:23.5px; padding-bottom:23.5px">
-                                                <img src="{{ asset('image/neteller.jpg') }}" class="img-responsive" alt="Project">
+                                            <div class="thumb-preview text-center" style="margin-top:10px">
+                                                <img src="{{ asset('image/asia_pay.png') }}" class="img-thumnail" alt="Project">
                                                 <div class="mg-thumb-options"></div>
                                             </div>
                                         </div>
