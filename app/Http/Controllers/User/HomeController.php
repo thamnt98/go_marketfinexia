5c5a1d0c01c89225dbab4cbdbea36cf7810a0692
thamnt98 <tham27011@gmail.com>
2021-05-01 17:10:37 +0700

    modify view home

--- app/Http/Controllers/User/HomeController.php
+++ app/Http/Controllers/User/HomeController.php
@@ -3,12 +3,53 @@
 namespace App\Http\Controllers\User;
 
 use App\Http\Controllers\Controller;
+use App\Models\LiveAccount;
+use App\Models\Order;
+use App\Models\WithdrawalFund;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class HomeController extends Controller
 {
     public function main()
     {
-        return view('home');
+        $userId = Auth::user()->id;
+        $fp = fsockopen(config('mt4.vps_ip'), config('mt4.vps_port'), $errno, $errstr, 6);
+        $logins = LiveAccount::where('user_id', $userId)->pluck('login');
+        $balances = [];
+        foreach ($logins as $login) {
+            $cmd = 'action=getaccountbalance&login=' . $login;
+            fwrite($fp, $cmd);
+            stream_set_timeout($fp, 1);
+            $result = '';
+            $info = stream_get_meta_data($fp);
+            while (!$info['timed_out'] && !feof($fp)) {
+                $str = @fgets($fp, 1024);
+                if (strpos($str, 'login')) {
+                    $result .= $str;
+                    $info = stream_get_meta_data($fp);
+                }
+            }
+            $result = explode('&', $result);
+            $balance = (int)(explode('=', $result[2])[1]);
+            $balances[$login] = $balance;
+        }
+        fclose($fp);
+        $fromDate = date('Y-m-d', strtotime(date('Y-m-d') . "-30 days"));
+        $orders = Order::where('user_id', $userId)
+            ->where('status', config('deposit.status.yes'))
+            ->whereBetween('created_at', [$fromDate, now()]);
+        $orderTotal = $orders->sum('amount_money');
+        $orders = $orders->groupBy('type')
+            ->selectRaw('sum(amount_money) as total,type')
+            ->get();
+        $withdrawals = WithdrawalFund::where('user_id', $userId)
+            ->where('status', config('deposit.status.yes'))
+            ->whereBetween('created_at', [$fromDate, now()]);
+        $withdrawalTotal = $withdrawals->sum('amount');
+        $withdrawals = $withdrawals->groupBy('login')
+            ->selectRaw('sum(amount) as total,login')
+            ->get();
+        return view('home', compact(['balances', 'orders', 'withdrawals', 'orderTotal', 'withdrawalTotal']));
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,72 +1,134 @@
 @extends('layouts.simplepage')
 @section('css')
-<link rel="stylesheet" href="{{ asset('css/bootstrap-table.css') }}" />
+    <link rel="stylesheet" href="{{ asset('css/bootstrap-table.css') }}"/>
+    <link rel="stylesheet" href="{{ asset('css/home.css') }}"/>
 @endsection
 <header class="page-header">
     @include('layouts.menutop')
 </header>
 @section('content')
-<div class="row">
-    <div class="col-lg-12">
-        <section class="panel">
-            <div class="panel-body" style="margin-top: -60px;">
-                <h2>Welcome to Gemifx</h2>
-                <p>Complete account management at your fingertips: from downloads, electronic deposits and withdrawals,
-                    to advanced partnership automation and analytics. Start by downloading our Trading Terminal for your
-                    computer and making a deposit.</p>
-            </div>
-        </section>
-        <section class="panel">
-            <header class="panel-heading">
-                <div class="panel-actions">
-                    <a href="#" class="fa fa-caret-down"></a>
+    <div class="container-fluid">
+        <ul class="index-links row an-mar-10"><a id="an-indexico0"
+                                                 href="{{ route('send.otp') }}">
+                <li class="col-xs-12 col-sm-6 col-md-6 col-lg-3 an-pad-10">
+                    <div class="insidebox">
+                        <div class="an-index-ico an-bgcolor1"><i class="fa fa-id-card-o" aria-hidden="true"></i></div>
+                        <span style="font-size: 15px;">Trader</span></div>
+                </li>
+            </a><a id="an-indexico1" href="{{route('deposit.funds')}}">
+                <li class="col-xs-12 col-sm-6 col-md-6 col-lg-3 an-pad-10">
+                    <div class="insidebox">
+                        <div class="an-index-ico an-bgcolor2"><i class="fa fa-line-chart" aria-hidden="true"></i></div>
+                        <span style="font-size: 15px;">Deposit</span></div>
+                </li>
+            </a><a id="an-indexico2" href="{{route('withdraw.funds')}}">
+                <li class="col-xs-12 col-sm-6 col-md-6 col-lg-3 an-pad-10">
+                    <div class="insidebox">
+                        <div class="an-index-ico an-bgcolor3"><i class="fa fa-bar-chart" aria-hidden="true"></i></div>
+                        <span style="font-size: 15px;">Withdrawal</span></div>
+                </li>
+            </a><a id="an-indexico3" href="{{route('account.MTPassword')}}">
+                <li class="col-xs-12 col-sm-6 col-md-6 col-lg-3 an-pad-10">
+                    <div class="insidebox">
+                        <div class="an-index-ico an-bgcolor4"><i class="fa fa-unlock-alt" aria-hidden="true"></i></div>
+                        <span style="font-size: 15px;">Change Password</span></div>
+                </li>
+            </a>
+            <div class="clear"></div>
+        </ul>
+        <div class="row">
+            <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
+                <div class="an-box widget-box an-topline" sort="23">
+                    <div class="widget-header"><h4>ACCOUNT LISTING</h4></div>
+                    <div class="widget-body index-table">
+                        <div class="widget-main no-padding table-box">
+                            <table class="table table-bordered table-striped an-index-table">
+                                <thead>
+                                <tr>
+                                    <th style="width:20%">Login</th>
+                                    <th style="width:20%">ACCOUNT TYPE</th>
+                                    <th style="width:20%">Balance</th>
+                                </tr>
+                                </thead>
+                                <tbody>
+                                @foreach($balances as $login => $balance)
+                                    <tr>
+                                        <td>{{ $login }}</td>
+                                        <td>{{ config('mt4.group.CWD-STD-B') }}</td>
+                                        <td>{{ $balance }}</td>
+                                    </tr>
+                                @endforeach
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
                 </div>
+            </div>
+        </div>
+        <div class="row gx-5">
+            <div class="col-md-6 mb-5">
+                <div class="card card-raised border-top border-4 border-secondary h-100">
+                    <div class="card-body p-5">
+                        <div class="overline text-muted mb-4">Money Out Last 30 Days</div>
+                        <h2 class="mb-4">
+                            $ {{ number_format($withdrawalTotal , 2, '.', ',') }}
+                        </h2>
+                        @foreach($withdrawals as $withdrawal)
+                            <div class="mb-2">
+                                <div class="mb-1">{{ $withdrawal->login }}</div>
+                                <div class="d-flex align-items-center">
+                                    <div class="progress w-100 me-3">
+                                        <div class="progress-bar bg-secondary" role="progressbar" style="width:{{ $withdrawalTotal ? (100*round($withdrawal->total / $withdrawalTotal, 2)) : 0 }}%"></div>
+                                    </div>
+                                    <div class="flex-shrink-0">${{ $withdrawal->total }}</div>
+                                </div>
+                            </div>
+                        @endforeach
+                    </div>
+                    <div class="card-footer bg-transparent position-relative ripple-gray mdc-ripple-upgraded">
+                        <a class="d-flex align-items-center justify-content-end text-decoration-none stretched-link text-primary"
+                           href="{{ route('trading.history') }}">
+                            <div class="fst-button">View All</div>
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6 mb-5">
+                <div class="card card-raised border-top border-4 border-success h-100">
+                    <div class="card-body p-5">
+                        <div class="overline text-muted mb-4">Money In Last 30 Days</div>
+                        <h2 class="mb-4">
+                            -$ {{ number_format($orderTotal , 2, '.', ',') }}
+                        </h2>
+                        @foreach($orders as $order)
+                            <div class="mb-2">
+                                <div class="mb-1">{{ config('deposit.type_text')[$order->type] }}</div>
+                                <div class="d-flex align-items-center">
+                                    <div class="progress w-100 me-3">
+                                        <div class="progress-bar bg-success" role="progressbar" style="width:{{ $orderTotal ? (100*round($order->total / $orderTotal, 2)) : 0 }}%"></div>
+                                    </div>
+                                    <div class="flex-shrink-0">{{ $order->total }}</div>
+                                </div>
+                            </div>
+                        @endforeach
 
-                <h2 class="panel-title">Recent Trading Activity</h2>
-            </header>
-            <div class="panel-body">
-                <div class="table-responsive">
-                    <table id="table" data-toggle="table" data-resizable="true">
-                        <thead>
-                            <tr>
-                                <th>Login</th>
-                                <th>Order</th>
-                                <th>Open Time</th>
-                                <th>Type</th>
-                                <th>Volume</th>
-                                <th>Symbol</th>
-                                <th>Open Price</th>
-                                <th>SL</th>
-                                <th>TP</th>
-                                <th>Close Time</th>
-                                <th>Close Price</th>
-                                <th>Commission</th>
-                                <th>Swap</th>
-                                <th>Profit</th>
-                                <th>Comment</th>
-                            </tr>
-                        </thead>
-                        <tbody></tbody>
-                    </table>
+                    </div>
+                    <div class="card-footer bg-transparent position-relative ripple-gray mdc-ripple-upgraded">
+                        <a class="d-flex align-items-center justify-content-end text-decoration-none stretched-link text-primary"
+                           href="{{ route('trading.history') }}">
+                            <div class="fst-button">View All</div>
+                        </a>
+                    </div>
                 </div>
             </div>
-        </section>
+        </div>
     </div>
-</div>
-<div class="footer" style="">
-    <p style="font-size:11px">Risk Warning Note: Forex and CFDs are leveraged products, incur a high level of risk and
-        may not be suitable for all investors. You should not risk more than you are prepared to lose. Before deciding
-        to trade, please ensure you understand the risks involved and take into account your level of experience. Seek
-        independent advice if necessary.
-        © 2020 Copyright GemiFx Inc. 2010-2020 All rights reserved. Various trademarks held by their respective owners.
-        One Financial Center, Suite 1000, Boston, MA, 02111, United States
-        <input type="hidden" class="level1-toggle" value="account" /></p>
-</div>
-<input type="hidden" class="level1-toggle" value="dashboard" />
-</section>
+
+    <input type="hidden" class="level1-toggle" value="dashboard"/>
+    </section>
 @endsection
 @section('js')
-<script src="{{ asset('js/bootstrap-table.js') }}"></script>
-<script src="{{ asset('js/data-table-active.js') }}"></script>
-<script src="{{ asset('js/bootstrap-table-resizable.js') }}"></script>
+    <script src="{{ asset('js/bootstrap-table.js') }}"></script>
+    <script src="{{ asset('js/data-table-active.js') }}"></script>
+    <script src="{{ asset('js/bootstrap-table-resizable.js') }}"></script>
 @endsection
