5c01fd9c3d25d8599b58dafd67bf60dc0b39b221
Nguyen Thi Tham <tham27011@gmail.com>
2021-06-29 17:51:39 +0700

    refactor code

--- app/Helper/MT5Helper.php
+++ app/Helper/MT5Helper.php
@@ -8,8 +8,8 @@ class MT5Helper
 {
     protected static $mt5Url = 'http://79.143.176.19:17014/ManagerAPIFOREX/';
 
-    protected $session = '';
-    protected $managerIndex = '';
+    protected $session = 'sfkja3eipso3';
+    protected $managerIndex = 101;
 
 
     private function connectMT5()
@@ -19,30 +19,29 @@ class MT5Helper
         $response = $client->request('GET', $endpoint);
         $result = json_decode($response->getBody());
         $this->session =  $result->Session;
-
-        $endpoint = self::$mt5Url . 'INITIAL_ADD_MANAGER';
-        $client = new Client([
-            'headers' => [
-                'Content-Type' => 'application/json',
-                'debug' => true
-            ]
-        ]);
-        $data = [
-            "ManagerID" => 1480,
-            "ManagerIndex" => 0,
-            "MT4_MT5" => 1,
-            "CreatedBy" => 1,
-            "oStatus" => 1,
-            "ServerConfig" => "174.142.252.29:443",
-            "ServerCode" => "Live",
-            "Password" => "G9istgg_",
-            "oDemo" => 1,
-            "Session" => $this->session
-        ];
-        $body = json_encode($data);
-        $response = $client->request('POST', $endpoint, ['body' => $body]);
-        $result = json_decode($response->getBody(), true);
-        $this->managerIndex =  $result['Result'];
+        // $endpoint = self::$mt5Url . 'INITIAL_ADD_MANAGER';
+        // $client = new Client([
+        //     'headers' => [
+        //         'Content-Type' => 'application/json',
+        //         'debug' => true
+        //     ]
+        // ]);
+        // $data = [
+        //     "ManagerID" => 1480,
+        //     "ManagerIndex" => 0,
+        //     "MT4_MT5" => 1,
+        //     "CreatedBy" => 1,
+        //     "oStatus" => 1,
+        //     "ServerConfig" => "174.142.252.29:443",
+        //     "ServerCode" => "Live",
+        //     "Password" => "G9istgg_",
+        //     "oDemo" => 1,
+        //     "Session" => $this->session
+        // ];
+        // $body = json_encode($data);
+        // $response = $client->request('POST', $endpoint, ['body' => $body]);
+        // $result = json_decode($response->getBody(), true);
+        // $this->managerIndex =  $result['Result'];
     }
 
     public function getGroups()
