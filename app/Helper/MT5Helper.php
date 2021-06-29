3a7eee38323a599958e5f1160ee524b85dd63879
Nguyen Thi Tham <tham27011@gmail.com>
2021-06-29 16:46:49 +0700

    refactor connect to mt5

--- app/Helper/MT5Helper.php
+++ app/Helper/MT5Helper.php
@@ -9,20 +9,46 @@ class MT5Helper
     protected static $mt5Url = 'http://79.143.176.19:17014/ManagerAPIFOREX/';
 
     protected $session = '';
+    protected $managerIndex = '';
 
 
-    private function connectMT5(){
+    private function connectMT5()
+    {
         $endpoint = self::$mt5Url . 'LOGIN_SESSION?Email=startingmt5broker@gmail.com&Password=rasa8r&Source=1';
         $client = new Client();
         $response = $client->request('GET', $endpoint);
         $result = json_decode($response->getBody());
         $this->session =  $result->Session;
+
+        $endpoint = self::$mt5Url . 'INITIAL_ADD_MANAGER';
+        $client = new Client([
+            'headers' => [
+                'Content-Type' => 'application/json',
+                'debug' => true
+            ]
+        ]);
+        $data = [
+            "ManagerID" => 1480,
+            "ManagerIndex" => 0,
+            "MT4_MT5" => 1,
+            "CreatedBy" => 1,
+            "oStatus" => 1,
+            "ServerConfig" => "174.142.252.29:443",
+            "ServerCode" => "Live",
+            "Password" => "G9istgg_",
+            "oDemo" => 1,
+            "Session" => $this->session
+        ];
+        $body = json_encode($data);
+        $response = $client->request('POST', $endpoint, ['body' => $body]);
+        $result = json_decode($response->getBody(), true);
+        $this->managerIndex =  $result->Result;
     }
 
     public function getGroups()
     {
         $this->connectMT5();
-        $endpoint = self::$mt5Url . 'GET_GROUPS?Session=' .$this->session. '&ManagerIndex=101';
+        $endpoint = self::$mt5Url . 'GET_GROUPS?Session=' . $this->session . '&ManagerIndex=' . $this->managerIndex;
         $client = new Client();
         $response = $client->request('GET', $endpoint);
         $result = json_decode($response->getBody());
@@ -31,6 +57,7 @@ class MT5Helper
 
     public function openAccount($data)
     {
+        $this->connectMT5();
         $endpoint = self::$mt5Url . 'ADD_MT_USER';
         $client = new Client([
             'headers' => [
@@ -38,22 +65,28 @@ class MT5Helper
                 'debug' => true
             ]
         ]);
+        $data ['ManagerIndex'] = $this->managerIndex;
+        $data ["Session"] = $this->session;
         $body = json_encode($data);
         $response = $client->request('POST', $endpoint, ['body' => $body]);
         $result = json_decode($response->getBody(), true);
         return $result;
     }
 
-    public function getAccountInfo($login){
-        $endpoint = self::$mt5Url . 'GET_USER_INFO?Session=' .$this->session. '&ManagerIndex=101&Account=' . $login;
+    public function getAccountInfo($login)
+    {
+        $this->connectMT5();
+        $endpoint = self::$mt5Url . 'GET_USER_INFO?Session=' . $this->session . '&ManagerIndex=' . $this->managerIndex . '&Account=' . $login;
         $client = new Client();
         $response = $client->request('GET', $endpoint);
         $result = json_decode($response->getBody());
         return $result;
     }
 
-    public function changeMasterPassword($data){
-        $endpoint = self::$mt5Url . 'CHANGE_MASTER_PASSWORD?Session=' .$this->session.  '&ManagerIndex=101&Account=' . $data['login'] . '&Password=' . $data['password'];
+    public function changeMasterPassword($data)
+    {
+        $this->connectMT5();
+        $endpoint = self::$mt5Url . 'CHANGE_MASTER_PASSWORD?Session=' . $this->session .  '&ManagerIndex=' . $this->managerIndex .'&Account=' . $data['login'] . '&Password=' . $data['password'];
         $client = new Client();
         $response = $client->request('GET', $endpoint);
         $result = json_decode($response->getBody());
