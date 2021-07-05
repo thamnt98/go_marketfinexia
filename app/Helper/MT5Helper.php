ebf599bdf2d7aa209835875ac97e001d0b6b9fb7
thamnt98 <tham27011@gmail.com>
2021-07-05 21:52:22 +0700

    refactor balance info

--- app/Helper/MT5Helper.php
+++ app/Helper/MT5Helper.php
@@ -44,7 +44,6 @@ class MT5Helper
     public function getAccountInfo($login)
     {
         $mt5 = self::getMT5Connect();
-        $login = '281422880';
         $endpoint = self::$mt5Url . 'GET_USER_INFO?Session=' . $mt5->session . '&ManagerIndex=' . $mt5->manager_index . '&Account=' . $login;
         $client = new Client();
         $response = $client->request('GET', $endpoint);
